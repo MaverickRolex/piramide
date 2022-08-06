b6f2eba881b9fd90ac8963f2a62a8ea80b959e05
MaverickRolex <roga.zero@gmail.com>
2022-08-05 19:49:30 -0500

    fix sale_new product search delete data prepare to new product

--- app/controllers/sale_products_controller.rb
+++ app/controllers/sale_products_controller.rb
@@ -3,14 +3,15 @@ class SaleProductsController < ApplicationController
   def create
     sale_product = SaleProduct.new(sale_product_params)
     sale_product.total_price = sale_product.quantity * sale_product.product.price
-    sale_product.save 
+    sale_product.save
+    redirect_to new_sale_path
   end
 
   def destroy
     @sale_product = SaleProduct.find(params[:id])
-    binding.pry
     if @sale_product.destroy
       flash.now[:notice] = "Producto eliminado con exito."
+      redirect_to new_sale_path
     end
   end
   
